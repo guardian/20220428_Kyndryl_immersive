ebc7b20408a0cf59794025ab4b48d12f69b4fb88
Zu <zu@netmek.biz>
2022-05-05 10:17:39 +1000

    tweets auto scroll

--- atoms/default/client/js/components.js
+++ atoms/default/client/js/components.js
@@ -30,6 +30,7 @@ const Tweet = ({data}) => {
             gsap.set(ref.current, {height: 'auto', delay: 0.8});
         });
     },[])
+    // const content = data.content.
     return (
             <div className="tweetbox" ref={ref}>
                 <PillBox className="">
@@ -38,7 +39,7 @@ const Tweet = ({data}) => {
                         <div>
                             <p>{data.label}</p>
                             <p className="handle">{data.handle}</p>
-                            <p dangerouslySetInnerHTML={setHtml(data.content)}></p>
+                            <p dangerouslySetInnerHTML={setHtml(data.content.replace(/(#\w+)/ig,`<span class="handle">$1</span>`))}></p>
                         </div>
                     </a>
                 </PillBox>
@@ -46,60 +47,68 @@ const Tweet = ({data}) => {
     )
 }
 
-export const TweetList = ({data}) => {
+export class TweetList extends Component {
+    ref = useRef();
+    timer = 0;
+    autoPlayTimeout = 4000;
 
-    const ref = useRef();
-    const dk = data.map((v, i) => {
-        v.key = `tweet${i}`;
-        return v;
-    });
-    // const dk = [...items];
-    const [curList, setCurList] = useState([]);
-    // const [curList, setCurList] = useState([...data]);
-    const [reserve, setReserve] = useState([]);
-
-    // console.log('>>tweets', curList, data)
-    const tweets = () => {
-        return curList.map((v,i)=><Tweet data={v} key={i} />);
+    componentWillUnmount() {
+        this.stopAutoPlay();
+    }
+    
+    componentWillReceiveProps (newProps) {       
+        const dk = newProps.data.map((v, i) => {
+            v.key = `tweet${i}`;
+            return v;
+        });        
+        this.setState({
+            curList: [...dk.slice(0,4)],
+            reserve: [...dk.slice(4)]
+        });
+        this.stopAutoPlay();
+        this.startAutoPlay();
     }
 
-    useEffect(()=>{
-        setCurList([...dk.slice(0,4)]);
-        setReserve([...dk.slice(4)]);
-    },data)
-    //move to top of list when new data is loaded
-
-
-    const settings = {
-        verticalMode: true,
-        itemsToShow: 4,
-        enableAutoPlay: false,
-        showArrows: false,
-        pagination: false
-
+    startAutoPlay = () => {
+        this.timer = setTimeout(this.autoPlayTick, this.autoPlayTimeout);
     }
 
-    const handleNext = () => {
+    autoPlayTick = () => {
+        this.gotoNextSlide();
+        this.timer = setTimeout(this.autoPlayTick, this.autoPlayTimeout);
+    }
+
+    stopAutoPlay = () => {
+        clearTimeout(this.timer);
+    }
+
+    handleNext = () => {
+        this.stopAutoPlay();
+        this.gotoNextSlide();
+    }
+
+    gotoNextSlide = () => {
         // e.preventDefault();
-        const cl = [...curList.slice(1), reserve[0]];
-        const rl = [...reserve.slice(1), curList[0]];
-        console.log(cl, rl);
-        setCurList([...cl]);
-        setReserve([...rl])
+        const cl = [...this.state.curList.slice(1), this.state.reserve[0]];
+        const rl = [...this.state.reserve.slice(1), this.state.curList[0]];
+        this.setState({curList: cl});
+        this.setState({reserve: rl});
     }
-    const handlePrev = (e) => {
+    handlePrev = (e) => {
         e.preventDefault();
-        const cl = [...reserve.slice(-1), ...curList.slice(0, -1)];
-        const rl = [...curList.slice(-1), ...reserve.slice(0, -1)];
-        
-        setCurList((s) => [...cl]);
-        setReserve(s=>[...rl])
+        this.stopAutoPlay();
+        const cl = [...this.state.reserve.slice(-1), ...this.state.curList.slice(0, -1)];
+        const rl = [...this.state.curList.slice(-1), ...this.state.reserve.slice(0, -1)];
+
+        this.setState({curList: cl});
+        this.setState({reserve: rl});        
     }
 
-
-    return (
+    render(props, {curList}) {
+        if (!curList) return null;
+        return (
         <div className="tweet-list">
-            <button className="btn primary btn-prev" onClick={handlePrev}><IconPrev /></button>
+            <button className="btn primary btn-prev" onClick={this.handlePrev}><IconPrev /></button>
             <div className="clear-float"></div>
             <TransitionGroup>
             {curList.map((v) => {
@@ -109,10 +118,6 @@ export const TweetList = ({data}) => {
                 onEnter={(n, appear) => {
                     if (!appear) {
                         gsap.to(n,{alpha: 1, duration: 0.5})
-                        // gsap.delayedCall(0,()=>gsap.from(n,{height: 0, duration:0.5}))
-                        // gsap.delayedCall(0, ()=>gsap.fromTo(n,{height: 0},{height: n.clientHeight, duration:0.5}))
-                        // gsap.to(n,{height: 'auto', duration: 0.5, delay: 0.5})
-                        // gsap.set(n,{className:'pillbox tweetbox', delay: 0.5})
                     }
                 }}
                 onExit={n=>gsap.to(n,{height: 0, alpha:0, duration: 0.5,padding: 0, margin: 0})}
@@ -125,7 +130,8 @@ export const TweetList = ({data}) => {
                 );
             })}
             </TransitionGroup>
-            <button className="btn primary btn-next" onClick={handleNext}><IconPrev /></button>
+            <button className="btn primary btn-next" onClick={this.handleNext}><IconPrev /></button>
         </div>
     )
+        }
 }
